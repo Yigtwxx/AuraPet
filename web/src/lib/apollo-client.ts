1cf82888d9e6ea5c0aa028f1778e489f8c6b748c
Yigtwxx <yigiterdogan023@gmail.com>
2026-05-12 10:59:54 +0300

    feat: implement Aurion pet visualization system and mobile architecture with Lottie animations

--- web/src/lib/apollo-client.ts
+++ web/src/lib/apollo-client.ts
@@ -1,10 +1,22 @@
-import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
+import { ApolloClient, from, HttpLink, InMemoryCache } from "@apollo/client";
+import { onError } from "@apollo/client/link/error";
+
+const errorLink = onError(({ graphQLErrors, networkError }) => {
+  if (graphQLErrors) {
+    graphQLErrors.forEach(({ message }) => {
+      console.error("[GraphQL error]", message);
+    });
+  }
+  if (networkError) {
+    console.error("[Network error]", networkError.message);
+  }
+});
 
 const httpLink = new HttpLink({
   uri: process.env.NEXT_PUBLIC_GRAPHQL_URL ?? "http://localhost:8000/graphql",
 });
 
 export const client = new ApolloClient({
-  link: httpLink,
+  link: from([errorLink, httpLink]),
   cache: new InMemoryCache(),
 });
